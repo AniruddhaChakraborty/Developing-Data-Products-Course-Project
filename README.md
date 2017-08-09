6477bdca7d8fde16ea3943e954b048c0403284bb
AniruddhaChakraborty <aniruddha.nsit@yahoo.com>
2017-08-09 22:25:24 +0530

    add all files

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Developing-Data-Products-Course-Project
+
+# [Shiny Application link.](https://aniruddhachakraborty.shinyapps.io/us_states_analysis/)
+
+# [Link to the Rpubs Reproducible Pitch presentation on the U.S. States Analysis App.](http://rpubs.com/AniruddhaChakraborty/298179)
+
+# [Link to the ui.R and server.R files.](https://github.com/AniruddhaChakraborty/Developing-Data-Products-Course-Project/US_States_Analysis)
+
+## Following instructions have been given for the assignment -
+
+This peer assessed assignment has two parts. First, you will create a Shiny application and deploy it on Rstudio's servers. Second, you will use Slidify or Rstudio Presenter to prepare a reproducible pitch presentation about your application.
+
+## Your Shiny Application
+
+1. Write a shiny application with associated supporting documentation. The documentation should be thought of as whatever a user will need to get started using your application.
+2. Deploy the application on Rstudio's shiny server
+3. Share the application link by pasting it into the provided text box
+4. Share your server.R and ui.R code on github
+
+## Your Reproducible Pitch Presentation
+
+OK, you've made your shiny app, now it's time to make your pitch. You get 5 slides (inclusive of the title slide) to pitch a your app. You're going to create a web page using Slidify or Rstudio Presenter with an html5 slide deck.
+
+Here's what you need
+
+1. 5 slides to pitch our idea done in Slidify or Rstudio Presenter
+2. Your presentation pushed to github or Rpubs
+3. A link to your github or Rpubs presentation pasted into the provided text box
+
+
+# My Shiny Application - [The U.S. States Analysis App](https://aniruddhachakraborty.shinyapps.io/us_states_analysis/)
+
+This Shiny Application developed as a part of Developing Data Products Course Project, on The **U.S. States Analysis** has the following main features ->  
+
+* **Interactive States Selection,** which allows users to select a group of U.S. states from the U.S. map interactively with the help of a Shiny attribute - **Brush**. The Analysis then filters as per the selected states.
+* **3 in 1 Datasets Analysis** allows users to further select a dataset out of 3 available datasets and perform Exploaratory Data Analysis in the areas of Education and Related Statistics, Public-School Expenditures and Violent Crime Rates by US States.  
+* **Cascade Input Filter Selection** - Based on the dataset selected, it allows user to further select a **Fill variable** in order to fill and analyze the U.S. states by that variable.
---
+++ US_States_Analysis/ui.R
@@ -0,0 +1,50 @@
+library(shiny)
+shinyUI(navbarPage("U.S. States Analysis",tabPanel("Interactive States Selection and Analysis",
+        sidebarLayout(
+                sidebarPanel(
+                        ## Input Control for dataset
+                        selectInput("dataset", "Choose type of Analysis for the U.S. States:",choices = c("Education and Related Statistics", "Public-School Expenditures", "Violent Crime Rates"),selected="Violent Crime Rates"),
+                        tags$hr(),
+
+                        ## Cascading Input filter for Fill-variable
+                        uiOutput("control")
+                ),
+                mainPanel(h3("Documentation :"),
+                          h4("1. Use the selective control in the left panel to select the type of Analysis (Dataset) for the U.S. States."),
+                          h4("2. Select a Fill Variable from the cascading filter : Radio-Buttons, to analyze the U.S states by that variable. "),
+                          h4("3. Select a group of states (more than one) from the below U.S. map interactively, by drawing/brushing a rectangle. Note - All states are selected initially, by default."),
+                          h4("4. The Map filled by the selected Variable and the data corresponding to the Selected / Brushed States as a Table Output will be displayed."),
+                          h4("5. " , a("ui.R & server.R files are in Aniruddha Chakraborty's Github Repository", href="https://github.com/AniruddhaChakraborty/Developing-Data-Products-Course-Project")),
+                          br(),
+                          tags$style(type="text/css",
+                                          ".shiny-output-error { visibility: hidden; }",
+                                          ".shiny-output-error:before { visibility: hidden; }"
+                          ),
+
+                          ## Display Title
+                          h3(textOutput("Title")),
+                          h3("Selcect States by drawing a Rectangle (Brushing)"),
+
+                          ## Setup the U.S. Map - Fill by selected variable
+                          plotOutput("Map",brush=brushOpts(id="brush1")),
+                          h4(textOutput("Mean")),
+                          br(),
+                          h4("You have currently selected the states in the STATES_SELECTED column given below. Note - All states are selected initially, by default."),
+
+                          ## Display Output
+                          tableOutput("view")
+                )
+        )),
+
+tabPanel("Documentation and R Code",mainPanel(h3("User Guide for Interactive States Selection and Analysis Tab:"),
+                                   h4("1. Use the selective control in the left panel to select the type of Analysis (Dataset) for the U.S. States:"),
+                                   h5("a) Education and Related Statistics - States dataset {car package}"),
+                                   h5("b) Public-School Expenditures - Anscombe dataset {car package}"),
+                                   h5("c) Violent Crime Rates by US States - USArrests dataset {datasets package}"),
+                                   h4("2. Select a Fill Variable from the cascading filter : Radio-Buttons, to analyze the U.S states by that variable. "),
+                                   h4("3. Select a group of states (more than one) from the U.S. map interactively, by drawing/brushing a rectangle. Note - All states are selected initially, by default."),
+                                   h4("4. The Map filled by the selected Variable and the data corresponding to the Selected / Brushed States as a Table Output will be displayed."),
+                                   h4("5. " , a("ui.R & server.R files are in Aniruddha Chakraborty's Github Repository", href="https://github.com/AniruddhaChakraborty/Developing-Data-Products-Course-Project")),
+                                   h4("6. ui.R & server.R codes can also be found in the below tabs.")
+)
+)))
